8510945952581ea6ecbf5c335810acfcb045df9b
Gabriel <gabriel.krapp@hotmail.com>
2024-05-02 20:21:55 -0300

    Creating the router

---
+++ internal/infra/payment_handler.go
@@ -0,0 +1,32 @@
+package infra
+
+import (
+	"encoding/json"
+	"net/http"
+	pi "payment-api/internal/entity"
+)
+
+func paymentHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Unsupported method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req struct {
+		Amount          int64  `json:"amount"`
+		Currency        string `json:"currency"`
+		PaymentMethodID string `json:"paymentMethodId"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Error decoding request body", http.StatusBadRequest)
+		return
+	}
+
+	pi, err := pi.CreatePaymentIntent(req.Amount, req.Currency, req.PaymentMethodID)
+	if err != nil {
+		http.Error(w, "Failed to create payment intent", http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(pi)
+}
