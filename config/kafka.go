92aace3bd9ef561e7120fdb92617634cab39e287
Gabriel <gabriel.krapp@hotmail.com>
2024-05-08 22:46:05 -0300

    feat: Creating the kafka consumer config

--- config/kafka.go
+++ config/kafka.go
@@ -18,3 +18,11 @@ func NewKafkaProducer(brokers []string) sarama.SyncProducer {
 	}
 	return producer
 }
+
+func NewKafkaConsumer(brokers []string, groupID string, config *sarama.Config) (sarama.ConsumerGroup, error) {
+	consumer, err := sarama.NewConsumerGroup(brokers, groupID, config)
+	if err != nil {
+		log.Fatalf("Error creating Kafka consumer group: %v", err)
+	}
+	return consumer, nil
+}
